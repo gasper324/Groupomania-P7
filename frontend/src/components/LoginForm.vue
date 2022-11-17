f0e3c88b9d177c6d1e99b234a22cc7c285d54923
Chris Gasper <gasper.chris@gmail.com>
2022-11-16 21:57:35 -0600

    POST requests
    Created POST request for login and start POST request for creating posts

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,10 +1,14 @@
 <template>
     <h2>Please Log-In</h2>
-    <form>
-        <label for="email">Email</label>
-        <input type="text" name="email">
-        <label for="password">Password</label>
-        <input type="text">
+    <form @submit.prevent="login">
+        <div>
+            <label for="email">Email</label>
+            <input type="text" name="email" v-model="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="text" name="password" v-model="password">
+        </div>
         <button>Sign in</button>
         <p>No account?  Click to sign-up</p>
     </form>
@@ -12,7 +16,19 @@
 
 <script>
 export default{
-    
+    methods: {
+        async login() {
+            console.log(this.email, this.password)
+            const loginData = {email: this.email, password: this.password}
+            const response = await fetch("http://localhost:3000/api/user/login", {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(loginData)
+                });
+            const data = await response.json()
+            console.log(data)
+        }
+    }
 }
 </script>
 <style scoped>
--- frontend/src/components/posts/CreatePost.vue
+++ frontend/src/components/posts/CreatePost.vue
@@ -1,11 +1,33 @@
 <template>
     <h2>Create a post</h2>
-    <form>
-        <label for="title">Post Title</label>
-        <input type="text" name="title">
-        <label for="img-desc">Please breifly describe the image you are posting</label>
-        <input type="text" name="img-desc">
-        <label for="text">Write your post here</label>
-        <input type="text" name="text">
+    <form @submit.prevent="createPost">
+        <div>
+            <label for="title">Post Title</label>
+            <input type="text" name="title" v-model="title">
+        </div>
+        <div>
+            <label for="img-desc">Please breifly describe the image you are posting</label>
+            <input type="text" name="img-desc" v-model="imgDesc">
+        </div>
+        <div>
+            <label for="text">Write your post here</label>
+            <input type="text" name="text" v-model="text">
+        </div>
+        <button>Post</button>
     </form>
 </template>
+
+<script>
+export default {
+    methods: {
+        createPost() {
+            const postdata = {
+                title: this.title,
+                description: this.imgDesc,
+                postText: this.text
+            }
+            console.log(postdata)
+        }
+    }
+}
+</script>
