d9ab7c5aae08c34163de0c83af257fd88078d619
Chris Gasper <gasper.chris@gmail.com>
2022-11-29 09:29:42 -0600

    Update multer-config.js
    removed console.logs

--- backend/API/src/middleware/multer-config.js
+++ backend/API/src/middleware/multer-config.js
@@ -13,8 +13,6 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
-    console.log(name)
-    console.log(file)
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
